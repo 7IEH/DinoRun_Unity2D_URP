431525d28e27ae622a795650357de87d402cebef
7IEH <youkitea@naver.com>
2023-10-24 18:07:19 +0900

    feat : Double Jump Limited

--- DinoRun/Assets/Scripts/Player.cs
+++ DinoRun/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
 //using UnityEngine.Windows;
 
@@ -15,12 +16,13 @@ public class Player : MonoBehaviour
     public int Speed;
     public eType Type;
     Rigidbody2D rigidbody;
+    public bool mJump;
 
     // Start is called before the first frame update
     void Start()
     {
         Type = eType.Idle;
-        rigidbody = GetComponent<Rigidbody2D>(); 
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -42,27 +44,38 @@ public class Player : MonoBehaviour
     void Idle()
     {
         float MoveX = 0f;
-        
-        if(Input.GetKey(KeyCode.A))
+
+        if (Input.GetKey(KeyCode.A))
         {
             MoveX -= Time.deltaTime * Speed;
         }
-        
-        if(Input.GetKey(KeyCode.D))
+
+        if (Input.GetKey(KeyCode.D))
         {
             MoveX += Time.deltaTime * Speed;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            Type = eType.Move;
+            if (!mJump)
+            {
+                Type = eType.Move;
+            }
         }
 
-        transform.Translate(new Vector3(MoveX,0,0));
+        transform.Translate(new Vector3(MoveX, 0, 0));
     }
     void Move()
     {
+        Debug.Log("JUMP");
         rigidbody.AddForce(transform.up * 300f);
         Type = eType.Idle;
+        mJump = true;
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Enter");
+        mJump = false;
+    }   
 }
